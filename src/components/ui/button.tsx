30373613c3b4eb4025bc62a8dab96bb97c6b7025
Nexio-Hub <TealStudiozfan7278@gmail.com>
2026-08-26 22:50:39 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -44,6 +44,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   },
 );
-Button.displayName = "Button";
+Button.displayName = "Send";
 
 export { Button, buttonVariants };
